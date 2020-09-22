e9f4a67c2cd3576020cb48c8a967868507eeb7c5
Yutong Zhang <zhangyt2000@gmail.com>
2020-09-22 13:14:54 -0700

    Modification to Makefile and driver

--- Makefile
+++ Makefile
@@ -4,10 +4,16 @@
 CC = g++
 CFLAGS = -Wall -g -std=c++11
 OBJ_DIR = obj
-OBJECTS = $(OBJ_DIR)/driver.o
+OBJECTS = $(OBJ_DIR)/vec3.o $(OBJ_DIR)/driver.o
 
-driver: $(OBJ_DIR)/driver.o
-	$(CC) $(CFLAGS) -o driver.exe $(OBJ_DIR)/driver.o
+driver: driver.exe
+
+driver.exe: $(OBJECTS)
+	$(CC) $(CFLAGS) -o driver.exe $(OBJECTS)
+
+$(OBJ_DIR)/vec3.o: vec3.hpp vec3.cpp | $(OBJ_DIR)
+	$(CC) $(CFLAGS) -c vec3.cpp
+	mv vec3.o $(OBJ_DIR)
 
 $(OBJ_DIR)/driver.o: driver.cpp | $(OBJ_DIR)
 	$(CC) $(CFLAGS) -c driver.cpp
--- driver.cpp
+++ driver.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 
+#include "vec3.hpp"
+
 int main(void) {
     int nx = 200;
     int ny = 100;
@@ -11,12 +13,10 @@ int main(void) {
 
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            vec3 pixel(float(i) / float(nx), float(j) / float(ny), 0.2);
+            int ir = int(255.99 * pixel.r());
+            int ig = int(255.99 * pixel.g());
+            int ib = int(255.99 * pixel.b());
             output << ir << " " << ig << " " << ib << "\n";
         }
     }
