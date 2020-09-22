eb87c267b550fbb84adaa0d3603f18c38ee00471
Yutong Zhang <zhangyt2000@gmail.com>
2020-09-22 14:44:59 -0700

    Complete ray class

---
+++ ray.hpp
@@ -0,0 +1,19 @@
+#ifndef RAY_HPP
+#define RAY_HPP
+
+#include "vec3.hpp"
+
+class ray {
+  private:
+    vec3 ori;
+    vec3 dir;
+
+  public:
+    ray() : ori(vec3()), dir(vec3()) {}
+    ray(const vec3& ori, const vec3& dir) : ori(ori), dir(dir) {}
+    vec3 origin() const { return ori; }
+    vec3 direction() const { return dir; }
+    vec3 point_at_parameter(double t) const { return ori + dir * t; }
+};
+
+#endif
