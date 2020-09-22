f7f482c0a6567249defc49401c038164ff77de34
Yutong Zhang <zhangyt2000@gmail.com>
2020-09-22 14:45:11 -0700

    Test ray in driver

--- Makefile
+++ Makefile
@@ -15,7 +15,7 @@ $(OBJ_DIR)/vec3.o: vec3.hpp vec3.cpp | $(OBJ_DIR)
 	$(CC) $(CFLAGS) -c vec3.cpp
 	mv vec3.o $(OBJ_DIR)
 
-$(OBJ_DIR)/driver.o: driver.cpp | $(OBJ_DIR)
+$(OBJ_DIR)/driver.o: vec3.hpp ray3.hpp driver.cpp | $(OBJ_DIR)
 	$(CC) $(CFLAGS) -c driver.cpp
 	mv driver.o $(OBJ_DIR)
 
--- driver.cpp
+++ driver.cpp
@@ -2,6 +2,13 @@
 #include <fstream>
 
 #include "vec3.hpp"
+#include "ray.hpp"
+
+vec3 color(const ray& r) {
+    vec3 unit_dir = unit_vector(r.direction());
+    double t = 0.5 * (unit_dir.y() + 1.0);
+    return vec3(1.0, 1.0, 1.0) * (1.0 - t) + vec3(0.2, 0.7, 1.0) * t;
+}
 
 int main(void) {
     int nx = 200;
@@ -11,9 +18,16 @@ int main(void) {
     output.open("output.ppm");
     output << "P3\n" << nx << " " << ny << "\n255\n";
 
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 origin(0.0, 0.0, 0.0);
+    vec3 horiz(4.0, 0.0, 0.0);
+    vec3 vert(0.0, 2.0, 0.0);
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            vec3 pixel(float(i) / float(nx), float(j) / float(ny), 0.2);
+            double u = double(i) / double(nx);
+            double v = double(j) / double(ny);
+            ray r(origin, lower_left_corner + horiz * u + vert * v);
+            vec3 pixel(color(r));
             int ir = int(255.99 * pixel.r());
             int ig = int(255.99 * pixel.g());
             int ib = int(255.99 * pixel.b());
